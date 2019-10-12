7dc61ce58d0207991c2b668a90273654b2a6a702
dnaz <d.nazarenkoff@gmail.com>
2019-10-12 23:17:41 +0300

    basic test class created, used chromeservice instead of chromedriver

--- src/test/java/HomeTaskTest.java
+++ src/test/java/HomeTaskTest.java
@@ -3,37 +3,33 @@ import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
-
 public class HomeTaskTest extends TestBase {
 
-         String login = "Nazarenkov27";
-         String pass = "d711815n";
-        String signInButtonLocator = "//a[@href = '/login' ]";
-        String signInFieldLocator = "//input[@name = 'login' ]";
-        String passwordFieldLocator = "//input[@name = 'password' ]";
-        String signInLocator = "//input[@type= 'submit']";
-        String detailsMenuLocator = "//summary[contains(@aria-label,'View profile')]";
-        String profileLinkLocator = "//a[@role='menuitem']/strong";
+    String login = "Nazarenkov27";
+    String pass = "d711815n";
+    String signInButtonLocator = "//a[@href = '/login' ]";
+    String signInFieldLocator = "//input[@name = 'login' ]";
+    String passwordFieldLocator = "//input[@name = 'password' ]";
+    String signInLocator = "//input[@type= 'submit']";
+    String detailsMenuLocator = "//summary[contains(@aria-label,'View profile')]";
+    String profileLinkLocator = "//a[@role='menuitem']/strong";
 
-        @Test
-        public void githubLoginTest()  {
+    @Test
+    public void githubLoginTest() {
 
-            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-            driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
-            driver.navigate().to("https://github.com/");
-            driver.findElement(By.xpath(signInButtonLocator)).click();
-            WebElement signInField = driver.findElement(By.xpath(signInFieldLocator));
-            signInField.clear();
-            signInField.sendKeys(login);
-            WebElement passwordField = driver.findElement(By.xpath(passwordFieldLocator));
-            passwordField.clear();
-            passwordField.sendKeys(pass);
-            driver.findElement(By.xpath(signInLocator)).click();
-            driver.findElement(By.xpath(detailsMenuLocator)).click();
-            WebElement profileLink = driver.findElement(By.xpath(profileLinkLocator));
-            Assert.assertEquals(login, profileLink.getText());
-
-             }
+        driver.navigate().to("https://github.com/");
+        driver.findElement(By.xpath(signInButtonLocator)).click();
+        WebElement signInField = driver.findElement(By.xpath(signInFieldLocator));
+        signInField.clear();
+        signInField.sendKeys(login);
+        WebElement passwordField = driver.findElement(By.xpath(passwordFieldLocator));
+        passwordField.clear();
+        passwordField.sendKeys(pass);
+        driver.findElement(By.xpath(signInLocator)).click();
+        driver.findElement(By.xpath(detailsMenuLocator)).click();
+        WebElement profileLink = driver.findElement(By.xpath(profileLinkLocator));
+        Assert.assertEquals(login, profileLink.getText());
 
     }
+
+}
--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -5,7 +5,9 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Listeners;
+
 import java.io.File;
+import java.util.concurrent.TimeUnit;
 
 @Listeners
 class TestBase {
@@ -14,14 +16,17 @@ class TestBase {
 
     @BeforeSuite
     public void before() {
-        File chromeDriver = new File ("d://chromedriver/chromedriver.exe");
+        File chromeDriver = new File("d://chromedriver/chromedriver.exe");
         ChromeDriverService service = new ChromeDriverService.Builder()
                 .usingDriverExecutable(chromeDriver)
                 .usingAnyFreePort()
                 .build();
         ChromeOptions options = new ChromeOptions().addArguments("--start-maximized"); //window size doesn't work here
         driver = new ChromeDriver(service, options);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
     }
+
     @AfterSuite
     public void tearDown() {
         driver.quit();
