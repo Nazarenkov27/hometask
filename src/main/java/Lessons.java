32d43935ee17b1ec340f899ff5fd81ed6b327af8
dmytro nazarenkov <d.nazarenkoff@gmail.com>
2020-01-03 22:15:02 +0200

    constructor correction

--- src/main/java/Lessons.java
+++ src/main/java/Lessons.java
@@ -5,15 +5,12 @@ public class Lessons {
     public static void main(String[] args) {
 
         int[] randomArray = getRandomArray();
-//        int[] nullArray = {1,2,3,4,5,6,7,8,9,10};
         Number one = new Number(randomArray);
-//        Number two = new Number(nullArray);
 
         System.out.println();
         System.out.println("The sum is: " + one.getSum());
         System.out.println("The sum with string: " + one.getSum("random!"));
         System.out.println("The sum with int: " + one.getSum(42));
-//        System.out.println(two.getSum());
     }
 
     public static int[] getRandomArray() {
