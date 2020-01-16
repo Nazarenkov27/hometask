b3d52a965a9453e02ce160bf5bcd9ab37fe48e82
dmytro nazarenkov <d.nazarenkoff@gmail.com>
2020-01-16 23:40:13 +0200

    screenshots on failed and skipped tests added

--- src/main/java/Task2.java
+++ src/main/java/Task2.java
@@ -1,4 +1,8 @@
-import java.io.*;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Task2 {
     /*
@@ -6,28 +10,25 @@ public class Task2 {
     два инпута:
     int
     char
-    write to file
     if (int < n){
     switch
     case "/" : print (or use while)
-    write it to file
     }
 
      */
-    public static String readAndWrite() throws IOException {
+    public static void printPyramid() throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("Enter number of lines: ");
-        String linesNumber =  reader.readLine();
+        int linesNumber = Integer.parseInt(reader.readLine());
         System.out.print("Enter symbol:  ");
-        String symbol =  reader.readLine();
-        BufferedWriter writer = new BufferedWriter(new FileWriter("target/task2.txt"));
-//        writer.write(str);
-        writer.append(linesNumber);
-        writer.close();
-        return linesNumber;
+        char symbol = reader.readLine().charAt(0);
+        for (int i = 1; i < linesNumber + 1; i++) {
+            String repeated = StringUtils.repeat(symbol, i);
+            System.out.println(repeated);
+        }
     }
 
     public static void main(String[] args) throws IOException {
-        System.out.println(readAndWrite());
+        printPyramid();
     }
 }
---
+++ src/main/java/Task3.java
@@ -0,0 +1,13 @@
+public class Task3 {
+/*
+Руками создаете файл lektorium1.txt.
+В него сами руками запишите число от 0 до 10.
+Задача - считать это число с файла, возвести его в квадрат и записать в фйал с названием lektorium2.txt.
+ */
+    public static void readAndWrite(){
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
