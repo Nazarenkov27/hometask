779a6d9ba07c9b67577ad6f820fd520a79a17fca
dmytro nazarenkov <d.nazarenkoff@gmail.com>
2020-02-03 22:02:23 +0200

    added profile for headless mode

--- src/main/java/com/qa/hometask/manageres/AppManager.java
+++ src/main/java/com/qa/hometask/manageres/AppManager.java
@@ -1,11 +1,11 @@
 package com.qa.hometask.manageres;
 
 import com.qa.hometask.helpers.*;
+import com.qa.hometask.utils.PropertyLoader;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
@@ -16,11 +16,12 @@ public class AppManager {
     private AttributesHelper attributesHelper;
     private RepoHelper repoHelper;
     private static WebDriver driver;
-    private WebDriverWait wait;
 
     AppManager() {
         WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions().addArguments("start-maximized");
+        ChromeOptions options = new ChromeOptions()
+                .addArguments("--window-size=1920,1080")
+                .addArguments(PropertyLoader.loadProperty("driver.props"));
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
